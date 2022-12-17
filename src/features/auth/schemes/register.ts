5837c1c3ace7a4c12fc7e748aafc15d929da5e99
Peter Solomon <petersolomon704@gmail.com>
2022-12-17 07:22:48 +0100

    Added register validation schema

---
+++ src/features/auth/schemes/register.ts
@@ -0,0 +1,29 @@
+import Joi, { ObjectSchema } from 'joi';
+
+const registerSchema: ObjectSchema = Joi.object().keys({
+  username: Joi.string().required().min(4).max(8).messages({
+    'string.base': 'Username must be of type string',
+    'string.min': 'Wrong credentials',
+    'string.max': 'Wrong credentials',
+    'string.empty': 'Username is a required field'
+  }),
+  password: Joi.string().required().min(4).max(8).messages({
+    'string.base': 'Password must be of type string',
+    'string.min': 'Wrong credentials',
+    'string.max': 'Wrong credentials',
+    'string.empty': 'Password is a required field'
+  }),
+  email: Joi.string().required().email().messages({
+    'string.base': 'Email must be of type string',
+    'string.email': 'Email must be valid',
+    'string.empty': 'Email is a required field'
+  }),
+  avatarColor: Joi.string().required().messages({
+    'any.required': 'Avatar color is required'
+  }),
+  avatarImage: Joi.string().required().messages({
+    'any.required': 'Avatar image is required'
+  })
+});
+
+export { registerSchema };
