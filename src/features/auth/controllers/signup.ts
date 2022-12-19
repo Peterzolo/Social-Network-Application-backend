f07b01cdce839d95ab83b9f75e30c0c3c3eac853
Peter Solomon <petersolomon704@gmail.com>
2022-12-19 01:29:04 +0100

    Modified

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -1,29 +1,20 @@
 import { ObjectId } from 'mongodb';
 import { Request, Response } from 'express';
-import { registerSchema } from '@auth/schemes/register';
 import { joiValidation } from '@global/typscript-decorator/joi-validation-decorator';
-import { BadRequestError } from '@global/helpers/customErrorHandler';
 import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth-interface';
 import { authService } from '@service/db/authService';
-import HTTP_STATUS from 'http-status-codes';
 import { Helpers } from '@global/helpers';
-// import { joiValidation } from '@global/decorators/joi-validation.decorators';
-// import { signupSchema } from '@auth/schemes/signup';
-// import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface';
-// import { authService } from '@service/db/auth.service';
-// import { Helpers } from '@global/helpers/helpers';
 import { UploadApiResponse } from 'cloudinary';
 import { uploads } from '@global/helpers/cloudinary-upload';
+import HTTP_STATUS from 'http-status-codes';
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { UserCache } from '@service/redis/userCache';
-// import HTTP_STATUS from 'http-status-codes';
-// import { IUserDocument } from '@user/interfaces/user.interface';
-// import { UserCache } from '@service/redis/user.cache';
-// import JWT from 'jsonwebtoken';
+import JWT from 'jsonwebtoken';
 // import { authQueue } from '@service/queues/auth.queue';
 // import { userQueue } from '@service/queues/user.queue';
-// import { config } from '@root/config';
-// import { BadRequestError } from '@global/helpers/error-handler';
+import { config } from '@root/configuration';
+import { BadRequestError } from '@global/helpers/customErrorHandler';
+import { registerSchema } from '@auth/schemes/register';
 
 const userCache: UserCache = new UserCache();
 
@@ -33,12 +24,15 @@ export class SignUp {
     const { username, email, password, avatarColor, avatarImage } = req.body;
     const checkIfUserExist: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
     if (checkIfUserExist) {
-      throw new BadRequestError('This user already exists');
+      throw new BadRequestError('User already exists');
     }
 
     const authObjectId: ObjectId = new ObjectId();
     const userObjectId: ObjectId = new ObjectId();
     const uId = `${Helpers.generateRandomIntegers(12)}`;
+    // the reason we are using SignUp.prototype.signupData and not this.signupData is because
+    // of how we invoke the create method in the routes method.
+    // the scope of the this object is not kept when the method is invoked
     const authData: IAuthDocument = SignUp.prototype.signupData({
       _id: authObjectId,
       uId,
@@ -52,7 +46,31 @@ export class SignUp {
       throw new BadRequestError('File upload: Error occurred. Try again.');
     }
 
-    res.status(HTTP_STATUS.CREATED).json({ Message: 'User created successfully', authData });
+    // Add to redis cache
+    const userDataForCache: IUserDocument = SignUp.prototype.userData(authData, userObjectId);
+    userDataForCache.profilePicture = `https://res.cloudinary.com/dyamr9ym3/image/upload/v${result.version}/${userObjectId}`;
+    await userCache.saveUserToCache(`${userObjectId}`, uId, userDataForCache);
+
+    // Add to database
+    // authQueue.addAuthUserJob('addAuthUserToDB', { value: authData });
+    // userQueue.addUserJob('addUserToDB', { value: userDataForCache });
+
+    const userJwt: string = SignUp.prototype.signToken(authData, userObjectId);
+    req.session = { jwt: userJwt };
+    res.status(HTTP_STATUS.CREATED).json({ message: 'User created successfully', user: userDataForCache, token: userJwt });
+  }
+
+  private signToken(data: IAuthDocument, userObjectId: ObjectId): string {
+    return JWT.sign(
+      {
+        userId: userObjectId,
+        uId: data.uId,
+        email: data.email,
+        username: data.username,
+        avatarColor: data.avatarColor
+      },
+      config.JWT_TOKEN_SECRET!
+    );
   }
 
   private signupData(data: ISignUpData): IAuthDocument {
@@ -67,33 +85,6 @@ export class SignUp {
       createdAt: new Date()
     } as IAuthDocument;
   }
-
-  // Add to redis cache
-  // const userDataForCache: IUserDocument = SignUp.prototype.userData(authData, userObjectId);
-  // userDataForCache.profilePicture = `https://res.cloudinary.com/dyamr9ym3/image/upload/v${result.version}/${userObjectId}`;
-  // await userCache.saveUserToCache(`${userObjectId}`, uId, userDataForCache);
-
-  // Add to database
-  // authQueue.addAuthUserJob('addAuthUserToDB', { value: authData });
-  // userQueue.addUserJob('addUserToDB', { value: userDataForCache });
-
-  //   const userJwt: string = SignUp.prototype.signToken(authData, userObjectId);
-  //   req.session = { jwt: userJwt };
-  //   res.status(HTTP_STATUS.CREATED).json({ message: 'User created successfully', user: userDataForCache, token: userJwt });
-  // }
-
-  // private signToken(data: IAuthDocument, userObjectId: ObjectId): string {
-  //   return JWT.sign(
-  //     {
-  //       userId: userObjectId,
-  //       uId: data.uId,
-  //       email: data.email,
-  //       username: data.username,
-  //       avatarColor: data.avatarColor
-  //     },
-  //     config.JWT_TOKEN!
-  //   );
-  // }
 
   private userData(data: IAuthDocument, userObjectId: ObjectId): IUserDocument {
     const { _id, username, email, uId, password, avatarColor } = data;
