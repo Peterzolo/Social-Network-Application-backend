fdfcb51af58fca3c5e85c935a288edc75482f820
Peter Solomon <petersolomon704@gmail.com>
2022-12-19 18:14:46 +0100

    Modified

--- src/shared/services/queues/BaseQueue.ts
+++ src/shared/services/queues/BaseQueue.ts
@@ -2,30 +2,31 @@ import Queue, { Job } from 'bull';
 import Logger from 'bunyan';
 import { createBullBoard } from '@bull-board/api';
 import { BullAdapter } from '@bull-board/api/bullAdapter';
+// import { ExpressAdapter } from '@bull-board/express';
 import { ExpressAdapter } from '@bull-board/express';
-import { config } from '@root/config';
-import { IAuthJob } from '@auth/interfaces/auth.interface';
+import { config } from '@root/configuration';
+import { IAuthJob } from '@auth/interfaces/auth-interface';
 import { IEmailJob, IUserJob } from '@user/interfaces/user.interface';
-import { IPostJobData } from '@post/interfaces/post.interface';
-import { IReactionJob } from '@reaction/interfaces/reaction.interface';
-import { ICommentJob } from '@comment/interfaces/comment.interface';
-import { IBlockedUserJobData, IFollowerJobData } from '@follower/interfaces/follower.interface';
-import { INotificationJobData } from '@notification/interfaces/notification.interface';
-import { IFileImageJobData } from '@image/interfaces/image.interface';
-import { IChatJobData, IMessageData } from '@chat/interfaces/chat.interface';
+// import { IPostJobData } from '@post/interfaces/post.interface';
+// import { IReactionJob } from '@reaction/interfaces/reaction.interface';
+// import { ICommentJob } from '@comment/interfaces/comment.interface';
+// import { IBlockedUserJobData, IFollowerJobData } from '@follower/interfaces/follower.interface';
+// import { INotificationJobData } from '@notification/interfaces/notification.interface';
+// import { IFileImageJobData } from '@image/interfaces/image.interface';
+// import { IChatJobData, IMessageData } from '@chat/interfaces/chat.interface';
 
 type IBaseJobData =
   | IAuthJob
   | IEmailJob
-  | IPostJobData
-  | IReactionJob
-  | ICommentJob
-  | IFollowerJobData
-  | IBlockedUserJobData
-  | INotificationJobData
-  | IFileImageJobData
-  | IChatJobData
-  | IMessageData
+  // | IPostJobData
+  // | IReactionJob
+  // | ICommentJob
+  // | IFollowerJobData
+  // | IBlockedUserJobData
+  // | INotificationJobData
+  // | IFileImageJobData
+  // | IChatJobData
+  // | IMessageData
   | IUserJob;
 
 let bullAdapters: BullAdapter[] = [];
