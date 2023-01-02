8f6cb495c02f2f2da5cfb93176d2e38a69744390
Peter Solomon <petersolomon704@gmail.com>
2023-01-02 11:42:40 +0100

    Modified comment cache

--- src/shared/services/redis/commentCache.ts
+++ src/shared/services/redis/commentCache.ts
@@ -30,64 +30,64 @@ export class CommentCache extends BaseCache {
     }
   }
 
-  // public async getCommentsFromCache(postId: string): Promise<ICommentDocument[]> {
-  //   try {
-  //     if (!this.client.isOpen) {
-  //       await this.client.connect();
-  //     }
-  //     const reply: string[] = await this.client.LRANGE(`comments:${postId}`, 0, -1);
-  //     const list: ICommentDocument[] = [];
-  //     for (const item of reply) {
-  //       list.push(Helpers.parseJson(item));
-  //     }
-  //     return list;
-  //   } catch (error) {
-  //     log.error(error);
-  //     throw new ServerError('Server error. Try again.');
-  //   }
-  // }
+  public async getCommentsFromCache(postId: string): Promise<ICommentDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const reply: string[] = await this.client.LRANGE(`comments:${postId}`, 0, -1);
+      const list: ICommentDocument[] = [];
+      for (const item of reply) {
+        list.push(Helpers.parseJson(item));
+      }
+      return list;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
 
-  // public async getCommentsNamesFromCache(postId: string): Promise<ICommentNameList[]> {
-  //   try {
-  //     if (!this.client.isOpen) {
-  //       await this.client.connect();
-  //     }
-  //     const commentsCount: number = await this.client.LLEN(`comments:${postId}`);
-  //     const comments: string[] = await this.client.LRANGE(`comments:${postId}`, 0, -1);
-  //     const list: string[] = [];
-  //     for (const item of comments) {
-  //       const comment: ICommentDocument = Helpers.parseJson(item) as ICommentDocument;
-  //       list.push(comment.username);
-  //     }
-  //     const response: ICommentNameList = {
-  //       count: commentsCount,
-  //       names: list
-  //     };
-  //     return [response];
-  //   } catch (error) {
-  //     log.error(error);
-  //     throw new ServerError('Server error. Try again.');
-  //   }
-  // }
+  public async getCommentsNamesFromCache(postId: string): Promise<ICommentNameList[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const commentsCount: number = await this.client.LLEN(`comments:${postId}`);
+      const comments: string[] = await this.client.LRANGE(`comments:${postId}`, 0, -1);
+      const list: string[] = [];
+      for (const item of comments) {
+        const comment: ICommentDocument = Helpers.parseJson(item) as ICommentDocument;
+        list.push(comment.username);
+      }
+      const response: ICommentNameList = {
+        count: commentsCount,
+        names: list
+      };
+      return [response];
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
 
-  // public async getSingleCommentFromCache(postId: string, commentId: string): Promise<ICommentDocument[]> {
-  //   try {
-  //     if (!this.client.isOpen) {
-  //       await this.client.connect();
-  //     }
-  //     const comments: string[] = await this.client.LRANGE(`comments:${postId}`, 0, -1);
-  //     const list: ICommentDocument[] = [];
-  //     for (const item of comments) {
-  //       list.push(Helpers.parseJson(item));
-  //     }
-  //     const result: ICommentDocument = find(list, (listItem: ICommentDocument) => {
-  //       return listItem._id === commentId;
-  //     }) as ICommentDocument;
+  public async getSingleCommentFromCache(postId: string, commentId: string): Promise<ICommentDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const comments: string[] = await this.client.LRANGE(`comments:${postId}`, 0, -1);
+      const list: ICommentDocument[] = [];
+      for (const item of comments) {
+        list.push(Helpers.parseJson(item));
+      }
+      const result: ICommentDocument = find(list, (listItem: ICommentDocument) => {
+        return listItem._id === commentId;
+      }) as ICommentDocument;
 
-  //     return [result];
-  //   } catch (error) {
-  //     log.error(error);
-  //     throw new ServerError('Server error. Try again.');
-  //   }
-  // }
+      return [result];
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
 }
