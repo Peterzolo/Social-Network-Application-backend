dd32227a240a2373350317b0216e8a61129e4828
Peter Solomon <petersolomon704@gmail.com>
2023-01-06 12:09:57 +0100

    Modified

--- src/features/followers/routes/followerRoutes.ts
+++ src/features/followers/routes/followerRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/authMiddleware';
 import { AddFollower } from '@follower/controllers/addFollowerController';
+import { unfollow } from '@follower/controllers/unfollowUser';
 
 // import { AddUser } from '@follower/controllers/block-user';
 
@@ -13,10 +14,10 @@ class FollowerRoutes {
 
   public routes(): Router {
     this.router.put('/user/follow/:followerId', authMiddleware.checkAuthentication, AddFollower.prototype.add);
+    this.router.put('/user/unfollow/:followeeId/:followerId', authMiddleware.checkAuthentication, unfollow.prototype.follower);
     // this.router.get('/user/following', authMiddleware.checkAuthentication, Get.prototype.userFollowing);
     // this.router.get('/user/followers/:userId', authMiddleware.checkAuthentication, Get.prototype.userFollowers);
 
-    // this.router.put('/user/unfollow/:followeeId/:followerId', authMiddleware.checkAuthentication, Remove.prototype.follower);
     // this.router.put('/user/block/:followerId', authMiddleware.checkAuthentication, AddUser.prototype.block);
     // this.router.put('/user/unblock/:followerId', authMiddleware.checkAuthentication, AddUser.prototype.unblock);
 
