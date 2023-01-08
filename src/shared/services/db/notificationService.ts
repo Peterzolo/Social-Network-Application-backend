23eacc0afc9f1dfa911d6f246bb346ec67f1af4b
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-08 20:40:08 +0100

    Update notificationService.ts
    Modified notification schema

--- src/shared/services/db/notificationService.ts
+++ src/shared/services/db/notificationService.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 class NotificationService {
   public async getNotifications(userId: string): Promise<INotificationDocument[]> {
     const notifications: INotificationDocument[] = await NotificationModel.aggregate([
+      // Display all notifications from notification collection by a user with this userId
       { $match: { userTo: new mongoose.Types.ObjectId(userId) } },
       { $lookup: { from: 'User', localField: 'userFrom', foreignField: '_id', as: 'userFrom' } },
       { $unwind: '$userFrom' },
