a09118df4a8868f46ae4bf6aed7288214635cac6
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-08 19:50:30 +0100

    Update notificationService.ts
    modified

--- src/shared/services/db/notificationService.ts
+++ src/shared/services/db/notificationService.ts
@@ -7,7 +7,7 @@ class NotificationService {
   public async getNotifications(userId: string): Promise<INotificationDocument[]> {
     const notifications: INotificationDocument[] = await NotificationModel.aggregate([
       { $match: { userTo: new mongoose.Types.ObjectId(userId) } },
-      // { $lookup: { from: 'User', localField: 'userFrom', foreignField: '_id', as: 'userFrom' } },
+      { $lookup: { from: 'User', localField: 'userFrom', foreignField: '_id', as: 'userFrom' } },
       // { $unwind: '$userFrom' },
       // { $lookup: { from: 'Auth', localField: 'userFrom.authId', foreignField: '_id', as: 'authId' } },
       // { $unwind: '$authId' },
