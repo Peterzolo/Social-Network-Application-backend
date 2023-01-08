7f6c68515e9496dcd70b1967afe96cf27055b7cf
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-08 19:35:11 +0100

    Update notificationInterface.ts
    Added InotificationDocument interface

--- src/features/notifications/interfaces/notificationInterface.ts
+++ src/features/notifications/interfaces/notificationInterface.ts
@@ -1,1 +1,20 @@
 import mongoose, { Document } from 'mongoose';
+
+export interface INotificationDocument extends Document {
+  _id?: mongoose.Types.ObjectId | string;
+  userTo: string;
+  userFrom: string;
+  message: string;
+  notificationType: string;
+  entityId: mongoose.Types.ObjectId;
+  createdItemId: mongoose.Types.ObjectId;
+  comment: string;
+  reaction: string;
+  post: string;
+  imgId: string;
+  imgVersion: string;
+  gifUrl: string;
+  read?: boolean;
+  createdAt?: Date;
+  insertNotification(data: INotification): Promise<void>;
+}
