9a64ae36d361020a298846de8312ca6cb690c662
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-08 20:10:51 +0100

    Update notificationService.ts
    Modified notification schema

--- src/shared/services/db/notificationService.ts
+++ src/shared/services/db/notificationService.ts
@@ -1,5 +1,5 @@
 import { INotificationDocument } from '@notification/interfaces/notificationInterface';
-// import { NotificationModel } from '@notification/models/notificationSchema';
+import { NotificationModel } from '@notification/models/notificationSchema';
 import mongoose from 'mongoose';
 
 
