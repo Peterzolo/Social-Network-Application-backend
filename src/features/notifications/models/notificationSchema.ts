7bc93986513014efbe42d1cab3735480274482c6
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-08 19:43:15 +0100

    Notification models
    Created notification schema

---
+++ src/features/notifications/models/notificationSchema.ts
@@ -0,0 +1,3 @@
+import { INotificationDocument, INotification } from '@notification/interfaces/notificationInterface';
+// import { notificationService } from '@service/db/notification.service';
+import mongoose, { model, Model, Schema } from 'mongoose';
