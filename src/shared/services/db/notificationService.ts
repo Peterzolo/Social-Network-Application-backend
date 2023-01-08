2684d364414ab472ff98b25f97142cff187da2b9
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-08 19:46:20 +0100

    Notification service
    Added notification service file

---
+++ src/shared/services/db/notificationService.ts
@@ -0,0 +1,3 @@
+import { INotificationDocument } from '@notification/interfaces/notificationInterface';
+// import { NotificationModel } from '@notification/models/notificationSchema';
+import mongoose from 'mongoose';
