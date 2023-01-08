7988fca16366a15889c8805a054301e9538697f2
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-08 20:00:15 +0100

    Update notificationSchema.ts
    Modified notification schema

--- src/features/notifications/models/notificationSchema.ts
+++ src/features/notifications/models/notificationSchema.ts
@@ -4,7 +4,7 @@ import mongoose, { model, Model, Schema } from 'mongoose';
 
 const notificationSchema: Schema = new Schema({
   userTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true }
-  // userFrom: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  userFrom: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   // read: { type: Boolean, default: false },
   // message: { type: String, default: '' },
   // notificationType: String,
