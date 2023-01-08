99a6e077906b342fa91aadd48549ecbc3bf3634e
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-08 19:55:10 +0100

    Update notificationService.ts
    modified

--- src/shared/services/db/notificationService.ts
+++ src/shared/services/db/notificationService.ts
@@ -43,9 +43,9 @@ class NotificationService {
     await NotificationModel.updateOne({ _id: notificationId }, { $set: { read: true } }).exec();
   }
 
-  // public async deleteNotification(notificationId: string): Promise<void> {
-  //   await NotificationModel.deleteOne({ _id: notificationId }).exec();
-  // }
+  public async deleteNotification(notificationId: string): Promise<void> {
+    await NotificationModel.deleteOne({ _id: notificationId }).exec();
+  }
 }
 
 export const notificationService: NotificationService = new NotificationService();
