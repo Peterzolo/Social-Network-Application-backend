503b50fe48d3f20ae80f8be98e82a4115e92bbbd
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-10 05:06:11 +0100

    Update emailQueues.ts
    Modified

--- src/shared/services/queues/emailQueues.ts
+++ src/shared/services/queues/emailQueues.ts
@@ -6,7 +6,7 @@ class EmailQueue extends BaseQueue {
   constructor() {
     super('emails');
     this.processJob('forgotPasswordEmail', 5, emailWorker.addNotificationEmail);
-    // this.processJob('commentsEmail', 5, emailWorker.addNotificationEmail);
+    this.processJob('commentsEmail', 5, emailWorker.addNotificationEmail);
     // this.processJob('followersEmail', 5, emailWorker.addNotificationEmail);
     // this.processJob('reactionsEmail', 5, emailWorker.addNotificationEmail);
     // this.processJob('directMessageEmail', 5, emailWorker.addNotificationEmail);
