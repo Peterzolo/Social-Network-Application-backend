b6be18a01dffda0888c515da82adfeb72f8c43a1
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-11 18:36:45 +0100

    Create userProfiles.ts
    imported modules

---
+++ src/features/user/controllers/userProfiles.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { FollowerCache } from '@service/redis/followerCache';
+import { PostCache } from '@service/redis/postCache';
+import { UserCache } from '@service/redis/userCache';
+import { IAllUsers, IUserDocument } from '@user/interfaces/user.interface';
+import { userService } from '@service/db/userService';
+import { IFollowerData } from '@follower/interfaces/followersInterface.ts';
+import { followerService } from '@service/db/followerService';
+import mongoose from 'mongoose';
+import { Helpers } from '@global/helpers';
+import { IPostDocument } from '@post/interfaces/postInterface';
+import { postService } from '@service/db/postService';
