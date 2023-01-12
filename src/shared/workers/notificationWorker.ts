48d7f80e68196c370454929c1e49f526314f4082
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-12 17:14:56 +0100

    Update notificationWorker.ts
    Modified

--- src/shared/workers/notificationWorker.ts
+++ src/shared/workers/notificationWorker.ts
@@ -18,17 +18,17 @@ class NotificationWorker {
     }
   }
 
-  // async deleteNotification(job: Job, done: DoneCallback): Promise<void> {
-  //   try {
-  //     const { key } = job.data;
-  //     await notificationService.deleteNotification(key);
-  //     job.progress(100);
-  //     done(null, job.data);
-  //   } catch (error) {
-  //     log.error(error);
-  //     done(error as Error);
-  //   }
-  // }
+  async deleteNotification(job: Job, done: DoneCallback): Promise<void> {
+    try {
+      const { key } = job.data;
+      await notificationService.deleteNotification(key);
+      job.progress(100);
+      done(null, job.data);
+    } catch (error) {
+      log.error(error);
+      done(error as Error);
+    }
+  }
 }
 
 export const notificationWorker: NotificationWorker = new NotificationWorker();
