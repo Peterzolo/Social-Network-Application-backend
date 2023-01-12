87a778a0f3348d678d16ec1b466488926f144709
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-12 17:50:45 +0100

    Update notificationRoutes.ts
    Modified

--- src/features/notifications/routes/notificationRoutes.ts
+++ src/features/notifications/routes/notificationRoutes.ts
@@ -12,7 +12,7 @@ class NotificationRoutes {
   }
 
   public routes(): Router {
-    this.router.get('/notifications', authMiddleware.checkAuthentication, Get.prototype.notifications);
+    this.router.get('/get-notifications', authMiddleware.checkAuthentication, Get.prototype.notifications);
     this.router.put('/update-notification/:notificationId', authMiddleware.checkAuthentication, Update.prototype.notification);
     this.router.delete('/delete-modification/:notificationId', authMiddleware.checkAuthentication, Delete.prototype.notification);
 
