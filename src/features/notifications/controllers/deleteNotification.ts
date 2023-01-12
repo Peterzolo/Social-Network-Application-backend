d0d218d7a9d333418ffb3134f1a8c1d527b1da04
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-12 17:28:38 +0100

    Update deleteNotification.ts
    Modified

--- src/features/notifications/controllers/deleteNotification.ts
+++ src/features/notifications/controllers/deleteNotification.ts
@@ -3,11 +3,11 @@ import { socketIONotificationObject } from '@socket/notificationSocket';
 import { Request, Response } from 'express';
 import HTTP_STATUS from 'http-status-codes';
 
-// export class Delete {
-//   public async notification(req: Request, res: Response): Promise<void> {
-//     const { notificationId } = req.params;
-//     socketIONotificationObject.emit('delete notification', notificationId);
-//     notificationQueue.addNotificationJob('deleteNotification', { key: notificationId });
-//     res.status(HTTP_STATUS.OK).json({ message: 'Notification deleted successfully' });
-//   }
+export class Delete {
+  public async notification(req: Request, res: Response): Promise<void> {
+    const { notificationId } = req.params;
+    socketIONotificationObject.emit('delete notification', notificationId);
+    notificationQueue.addNotificationJob('deleteNotification', { key: notificationId });
+    res.status(HTTP_STATUS.OK).json({ message: 'Notification deleted successfully' });
+  }
 }
