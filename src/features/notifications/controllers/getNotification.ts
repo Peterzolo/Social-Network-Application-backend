3516b0ebb9f11432c933f6d9731127af2c02b085
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-12 17:33:06 +0100

    Update getNotification.ts
    Modified

--- src/features/notifications/controllers/getNotification.ts
+++ src/features/notifications/controllers/getNotification.ts
@@ -3,9 +3,9 @@ import { notificationService } from '@service/db/notificationService';
 import { Request, Response } from 'express';
 import HTTP_STATUS from 'http-status-codes';
 
-// export class Get {
-//   public async notifications(req: Request, res: Response): Promise<void> {
-//     const notifications: INotificationDocument[] = await notificationService.getNotifications(req.currentUser!.userId);
-//     res.status(HTTP_STATUS.OK).json({ message: 'User notifications', notifications });
-//   }
+export class Get {
+  public async notifications(req: Request, res: Response): Promise<void> {
+    const notifications: INotificationDocument[] = await notificationService.getNotifications(req.currentUser!.userId);
+    res.status(HTTP_STATUS.OK).json({ message: 'User notifications', notifications });
+  }
 }
