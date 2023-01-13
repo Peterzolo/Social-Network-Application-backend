4f398802737b560395563c580ca2e17e29d644ba
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-13 14:10:44 +0100

    Create addImage.ts
    Added addImage controller

---
+++ src/features/images/controllers/addImage.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { UserCache } from '@service/redis/userCache';
+import { joiValidation } from '@global/typscript-decorator/joi-validation-decorator';
+import { addImageSchema } from '@image/schemes/imageValidationScheme';
+import { uploads } from '@global/helpers/cloudinary-upload';
+import { UploadApiResponse } from 'cloudinary';
+import { BadRequestError } from '@global/helpers/customErrorHandler';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { socketIOImageObject } from '@socket/imageSocket';
+import { imageQueue } from '@service/queues/imageQueue';
+import { IBgUploadResponse } from '@image/interfaces/imageInterface';
+import { Helpers } from '@global/helpers';
