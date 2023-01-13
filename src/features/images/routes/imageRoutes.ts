f6d75a0075c7e27c1f05c54af858e563667a58ca
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-13 15:18:41 +0100

    Update imageRoutes.ts
    Modified

--- src/features/images/routes/imageRoutes.ts
+++ src/features/images/routes/imageRoutes.ts
@@ -12,8 +12,8 @@ class ImageRoutes {
   }
 
   public routes(): Router {
-    this.router.post('/images/profile', authMiddleware.checkAuthentication, Add.prototype.profileImage);
-    this.router.post('/images/background', authMiddleware.checkAuthentication, Add.prototype.backgroundImage);
+    this.router.post('/images/profile-add', authMiddleware.checkAuthentication, Add.prototype.profileImage);
+    this.router.post('/images/background-add', authMiddleware.checkAuthentication, Add.prototype.backgroundImage);
     // this.router.get('/images/:userId', authMiddleware.checkAuthentication, Get.prototype.images);
     // this.router.delete('/images/:imageId', authMiddleware.checkAuthentication, Delete.prototype.image);
     // this.router.delete('/images/background/:bgImageId', authMiddleware.checkAuthentication, Delete.prototype.backgroundImage);
