a1a28ce25f00090d556d7300a77605374a73ef49
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-13 13:32:48 +0100

    Update imageQueue.ts
    Modified

--- src/shared/services/queues/imageQueue.ts
+++ src/shared/services/queues/imageQueue.ts
@@ -4,12 +4,12 @@ import { imageWorker } from '@worker/imageWorkers';
 
 class ImageQueue extends BaseQueue {
   constructor() {
-  //   super('images');
-  //   this.processJob('addUserProfileImageToDB', 5, imageWorker.addUserProfileImageToDB);
-  //   this.processJob('updateBGImageInDB', 5, imageWorker.updateBGImageInDB);
-  //   this.processJob('addImageToDB', 5, imageWorker.addImageToDB);
-  //   this.processJob('removeImageFromDB', 5, imageWorker.removeImageFromDB);
-  // }
+    super('images');
+    this.processJob('addUserProfileImageToDB', 5, imageWorker.addUserProfileImageToDB);
+    this.processJob('updateBGImageInDB', 5, imageWorker.updateBGImageInDB);
+    this.processJob('addImageToDB', 5, imageWorker.addImageToDB);
+    this.processJob('removeImageFromDB', 5, imageWorker.removeImageFromDB);
+  }
 
   // public addImageJob(name: string, data: IFileImageJobData): void {
   //   this.addJob(name, data);
