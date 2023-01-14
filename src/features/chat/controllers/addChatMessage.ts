fb5380df6f1a1aaccb571b9ec43a3d06f3653377
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-14 16:59:32 +0100

    Update addChatMessage.ts
    Modified

--- src/features/chat/controllers/addChatMessage.ts
+++ src/features/chat/controllers/addChatMessage.ts
@@ -86,11 +86,11 @@ export class Add {
     chatQueue.addChatJob('addChatMessageToDB', messageData);
     res.status(HTTP_STATUS.OK).json({ message: 'Message added', conversationId: conversationObjectId });
   }
-  // public async addChatUsers(req: Request, res: Response): Promise<void> {
-  //   const chatUsers = await messageCache.addChatUsersToCache(req.body);
-  //   socketIOChatObject.emit('add chat users', chatUsers);
-  //   res.status(HTTP_STATUS.OK).json({ message: 'Users added' });
-  // }
+  public async addChatUsers(req: Request, res: Response): Promise<void> {
+    const chatUsers = await messageCache.addChatUsersToCache(req.body);
+    socketIOChatObject.emit('add chat users', chatUsers);
+    res.status(HTTP_STATUS.OK).json({ message: 'Users added' });
+  }
   public async removeChatUsers(req: Request, res: Response): Promise<void> {
     const chatUsers = await messageCache.removeChatUsersFromCache(req.body);
     socketIOChatObject.emit('add chat users', chatUsers);
