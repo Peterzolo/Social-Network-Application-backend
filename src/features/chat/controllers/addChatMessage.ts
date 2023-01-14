e78e7f0ffa3720cadbd61f8a3346b408113af0d8
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-14 20:00:16 +0100

    Update addChatMessage.ts
    Modified

--- src/features/chat/controllers/addChatMessage.ts
+++ src/features/chat/controllers/addChatMessage.ts
@@ -82,7 +82,7 @@ export class Add {
     }
     await messageCache.addChatListToCache(`${req.currentUser!.userId}`, `${receiverId}`, `${conversationObjectId}`);
     await messageCache.addChatListToCache(`${receiverId}`, `${req.currentUser!.userId}`, `${conversationObjectId}`);
-    // await messageCache.addChatMessageToCache(`${conversationObjectId}`, messageData);
+    await messageCache.addChatMessageToCache(`${conversationObjectId}`, messageData);
     // chatQueue.addChatJob('addChatMessageToDB', messageData);
     res.status(HTTP_STATUS.OK).json({ message: 'Message added', conversationId: conversationObjectId });
   }
