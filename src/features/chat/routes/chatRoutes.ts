f2e335bf230d6a30c7fa2fad54422f3637ac5c0b
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-15 18:52:18 +0100

    Update chatRoutes.ts
    Added add and remove chat users

--- src/features/chat/routes/chatRoutes.ts
+++ src/features/chat/routes/chatRoutes.ts
@@ -15,10 +15,10 @@ class ChatRoutes {
 
   public routes(): Router {
     this.router.post('/chat/message/add', authMiddleware.checkAuthentication, Add.prototype.message);
+    this.router.post('/chat/message/add-chat-users', authMiddleware.checkAuthentication, Add.prototype.addChatUsers);
+    this.router.post('/chat/message/remove-chat-users', authMiddleware.checkAuthentication, Add.prototype.removeChatUsers);
     // this.router.get('/chat/message/user/:receiverId', authMiddleware.checkAuthentication, Get.prototype.messages);
     // this.router.get('/chat/message/conversation-list', authMiddleware.checkAuthentication, Get.prototype.conversationList);
-    // this.router.post('/chat/message/add-chat-users', authMiddleware.checkAuthentication, Add.prototype.addChatUsers);
-    // this.router.post('/chat/message/remove-chat-users', authMiddleware.checkAuthentication, Add.prototype.removeChatUsers);
     // this.router.put('/chat/message/mark-as-read', authMiddleware.checkAuthentication, Update.prototype.message);
     // this.router.put('/chat/message/reaction', authMiddleware.checkAuthentication, Message.prototype.reaction);
     // this.router.delete(
