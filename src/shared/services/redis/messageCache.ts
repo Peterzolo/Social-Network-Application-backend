baaac06e6786afe6ee5ac9157a2d96df19f38f1a
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-15 19:50:50 +0100

    Update messageCache.ts
    Added getUserConversationList from cache

--- src/shared/services/redis/messageCache.ts
+++ src/shared/services/redis/messageCache.ts
@@ -88,24 +88,24 @@ export class MessageCache extends BaseCache {
     }
   }
 
-  // public async getUserConversationList(key: string): Promise<IMessageData[]> {
-  //   try {
-  //     if (!this.client.isOpen) {
-  //       await this.client.connect();
-  //     }
-  //     const userChatList: string[] = await this.client.LRANGE(`chatList:${key}`, 0, -1);
-  //     const conversationChatList: IMessageData[] = [];
-  //     for (const item of userChatList) {
-  //       const chatItem: IChatList = Helpers.parseJson(item) as IChatList;
-  //       const lastMessage: string = (await this.client.LINDEX(`messages:${chatItem.conversationId}`, -1)) as string;
-  //       conversationChatList.push(Helpers.parseJson(lastMessage));
-  //     }
-  //     return conversationChatList;
-  //   } catch (error) {
-  //     log.error(error);
-  //     throw new ServerError('Server error. Try again.');
-  //   }
-  // }
+  public async getUserConversationList(key: string): Promise<IMessageData[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const userChatList: string[] = await this.client.LRANGE(`chatList:${key}`, 0, -1);
+      const conversationChatList: IMessageData[] = [];
+      for (const item of userChatList) {
+        const chatItem: IChatList = Helpers.parseJson(item) as IChatList;
+        const lastMessage: string = (await this.client.LINDEX(`messages:${chatItem.conversationId}`, -1)) as string;
+        conversationChatList.push(Helpers.parseJson(lastMessage));
+      }
+      return conversationChatList;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
 
   // public async getChatMessagesFromCache(senderId: string, receiverId: string): Promise<IMessageData[]> {
   //   try {
