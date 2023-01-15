2def8dbf2cd8db49ac2d6e1ae5cc18f23ca5f295
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-15 19:18:43 +0100

    Update chatWorkers.ts
    Added addChatMessageToDB method

--- src/shared/workers/chatWorkers.ts
+++ src/shared/workers/chatWorkers.ts
@@ -6,16 +6,16 @@ import Logger from 'bunyan';
 const log: Logger = config.createLogger('chatWorker');
 
 class ChatWorker {
-  //   async addChatMessageToDB(jobQueue: Job, done: DoneCallback): Promise<void> {
-  //     try {
-  //       await chatService.addMessageToDB(jobQueue.data);
-  //       jobQueue.progress(100);
-  //       done(null, jobQueue.data);
-  //     } catch (error) {
-  //       log.error(error);
-  //       done(error as Error);
-  //     }
-  //   }
+  async addChatMessageToDB(jobQueue: Job, done: DoneCallback): Promise<void> {
+    try {
+      await chatService.addMessageToDB(jobQueue.data);
+      jobQueue.progress(100);
+      done(null, jobQueue.data);
+    } catch (error) {
+      log.error(error);
+      done(error as Error);
+    }
+  }
   // async markMessageAsDeleted(jobQueue: Job, done: DoneCallback): Promise<void> {
   //   try {
   //     const { messageId, type } = jobQueue.data;
