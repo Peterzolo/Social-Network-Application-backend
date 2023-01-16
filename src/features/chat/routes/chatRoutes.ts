9d7e1b54bcf0d4c0e27220bbd5af0fd3f0f62132
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-16 17:10:08 +0100

    Update chatRoutes.ts
    Modified

--- src/features/chat/routes/chatRoutes.ts
+++ src/features/chat/routes/chatRoutes.ts
@@ -3,7 +3,8 @@ import { authMiddleware } from '@global/helpers/authMiddleware';
 import { Add } from '@chat/controllers/addChatMessage';
 import { Get } from '@chat/controllers/fetchChatList';
 import { Delete } from '@chat/controllers/deleteChatMessage';
-// import { Update } from '@chat/controllers/update-chat-message';
+import { Update } from '@chat/controllers/updateReadMessageStatus';
+
 // import { Message } from '@chat/controllers/add-message-reaction';
 
 class ChatRoutes {
@@ -19,7 +20,7 @@ class ChatRoutes {
     this.router.post('/chat/message/remove-chat-users', authMiddleware.checkAuthentication, Add.prototype.removeChatUsers);
     this.router.get('/chat/message/conversation-list', authMiddleware.checkAuthentication, Get.prototype.conversationList);
     this.router.get('/chat/message/user/:receiverId', authMiddleware.checkAuthentication, Get.prototype.messages);
-    // this.router.put('/chat/message/mark-as-read', authMiddleware.checkAuthentication, Update.prototype.message);
+    this.router.put('/chat/message/mark-as-read', authMiddleware.checkAuthentication, Update.prototype.messageReadStatus);
     // this.router.put('/chat/message/reaction', authMiddleware.checkAuthentication, Message.prototype.reaction);
     this.router.delete(
       '/chat/message/mark-as-deleted/:messageId/:senderId/:receiverId/:type',
