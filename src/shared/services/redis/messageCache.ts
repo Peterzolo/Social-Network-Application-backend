b4208839e8ce8cd21243cf26b4f15ae7288062ec
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-16 15:48:59 +0100

    Update messageCache.ts
    Added  markMessageAsDeleted

--- src/shared/services/redis/messageCache.ts
+++ src/shared/services/redis/messageCache.ts
@@ -132,28 +132,28 @@ export class MessageCache extends BaseCache {
     }
   }
 
-  // public async markMessageAsDeleted(senderId: string, receiverId: string, messageId: string, type: string): Promise<IMessageData> {
-  //   try {
-  //     if (!this.client.isOpen) {
-  //       await this.client.connect();
-  //     }
-  //     const { index, message, receiver } = await this.getMessage(senderId, receiverId, messageId);
-  //     const chatItem = Helpers.parseJson(message) as IMessageData;
-  //     if (type === 'deleteForMe') {
-  //       chatItem.deleteForMe = true;
-  //     } else {
-  //       chatItem.deleteForMe = true;
-  //       chatItem.deleteForEveryone = true;
-  //     }
-  //     await this.client.LSET(`messages:${receiver.conversationId}`, index, JSON.stringify(chatItem));
+  public async markMessageAsDeleted(senderId: string, receiverId: string, messageId: string, type: string): Promise<IMessageData> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const { index, message, receiver } = await this.getMessage(senderId, receiverId, messageId);
+      const chatItem = Helpers.parseJson(message) as IMessageData;
+      if (type === 'deleteForMe') {
+        chatItem.deleteForMe = true;
+      } else {
+        chatItem.deleteForMe = true;
+        chatItem.deleteForEveryone = true;
+      }
+      await this.client.LSET(`messages:${receiver.conversationId}`, index, JSON.stringify(chatItem));
 
-  //     const lastMessage: string = (await this.client.LINDEX(`messages:${receiver.conversationId}`, index)) as string;
-  //     return Helpers.parseJson(lastMessage) as IMessageData;
-  //   } catch (error) {
-  //     log.error(error);
-  //     throw new ServerError('Server error. Try again.');
-  //   }
-  // }
+      const lastMessage: string = (await this.client.LINDEX(`messages:${receiver.conversationId}`, index)) as string;
+      return Helpers.parseJson(lastMessage) as IMessageData;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
 
   // public async updateChatMessages(senderId: string, receiverId: string): Promise<IMessageData> {
   //   try {
