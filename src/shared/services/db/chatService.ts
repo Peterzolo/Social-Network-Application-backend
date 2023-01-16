30358eb438100e1884553fae8c29bc40317c833b
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-16 16:56:19 +0100

    Update chatService.ts
    Added markMessagesAsRead method

--- src/shared/services/db/chatService.ts
+++ src/shared/services/db/chatService.ts
@@ -83,15 +83,16 @@ class ChatService {
       await MessageModel.updateOne({ _id: messageId }, { $set: { deleteForMe: true, deleteForEveryone: true } }).exec();
     }
   }
-  // public async markMessagesAsRead(senderId: ObjectId, receiverId: ObjectId): Promise<void> {
-  //   const query = {
-  //     $or: [
-  //       { senderId, receiverId, isRead: false },
-  //       { senderId: receiverId, receiverId: senderId, isRead: false }
-  //     ]
-  //   };
-  //   await MessageModel.updateMany(query, { $set: { isRead: true } }).exec();
-  // }
+  public async markMessagesAsRead(senderId: ObjectId, receiverId: ObjectId): Promise<void> {
+    const query = {
+      $or: [
+        { senderId, receiverId, isRead: false },
+        { senderId: receiverId, receiverId: senderId, isRead: false }
+      ]
+    };
+    await MessageModel.updateMany(query, { $set: { isRead: true } }).exec();
+  }
+
   // public async updateMessageReaction(messageId: ObjectId, senderName: string, reaction: string, type: 'add' | 'remove'): Promise<void> {
   //   if (type === 'add') {
   //     await MessageModel.updateOne({ _id: messageId }, { $push: { reaction: { senderName, type: reaction } } }).exec();
