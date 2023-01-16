66832e1da25235248a6dad83f6836134368f2e39
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-16 14:54:49 +0100

    message update
    message getChatMessagesFromCache

--- src/shared/services/redis/messageCache.ts
+++ src/shared/services/redis/messageCache.ts
@@ -107,30 +107,30 @@ export class MessageCache extends BaseCache {
     }
   }
 
-  // public async getChatMessagesFromCache(senderId: string, receiverId: string): Promise<IMessageData[]> {
-  //   try {
-  //     if (!this.client.isOpen) {
-  //       await this.client.connect();
-  //     }
-  //     const userChatList: string[] = await this.client.LRANGE(`chatList:${senderId}`, 0, -1);
-  //     const receiver: string = find(userChatList, (listItem: string) => listItem.includes(receiverId)) as string;
-  //     const parsedReceiver: IChatList = Helpers.parseJson(receiver) as IChatList;
-  //     if (parsedReceiver) {
-  //       const userMessages: string[] = await this.client.LRANGE(`messages:${parsedReceiver.conversationId}`, 0, -1);
-  //       const chatMessages: IMessageData[] = [];
-  //       for (const item of userMessages) {
-  //         const chatItem = Helpers.parseJson(item) as IMessageData;
-  //         chatMessages.push(chatItem);
-  //       }
-  //       return chatMessages;
-  //     } else {
-  //       return [];
-  //     }
-  //   } catch (error) {
-  //     log.error(error);
-  //     throw new ServerError('Server error. Try again.');
-  //   }
-  // }
+  public async getChatMessagesFromCache(senderId: string, receiverId: string): Promise<IMessageData[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const userChatList: string[] = await this.client.LRANGE(`chatList:${senderId}`, 0, -1);
+      const receiver: string = find(userChatList, (listItem: string) => listItem.includes(receiverId)) as string;
+      const parsedReceiver: IChatList = Helpers.parseJson(receiver) as IChatList;
+      if (parsedReceiver) {
+        const userMessages: string[] = await this.client.LRANGE(`messages:${parsedReceiver.conversationId}`, 0, -1);
+        const chatMessages: IMessageData[] = [];
+        for (const item of userMessages) {
+          const chatItem = Helpers.parseJson(item) as IMessageData;
+          chatMessages.push(chatItem);
+        }
+        return chatMessages;
+      } else {
+        return [];
+      }
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
 
   // public async markMessageAsDeleted(senderId: string, receiverId: string, messageId: string, type: string): Promise<IMessageData> {
   //   try {
