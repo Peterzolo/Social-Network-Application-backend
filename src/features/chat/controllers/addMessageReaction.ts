f4157bd740fc2248eeb19a70f5e798c55a3e8bc9
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-17 13:24:52 +0100

    Update addMessageReaction.ts
    Modified

--- src/features/chat/controllers/addMessageReaction.ts
+++ src/features/chat/controllers/addMessageReaction.ts
@@ -8,16 +8,16 @@ import { chatQueue } from '@service/queues/chatQueue';
 
 const messageCache: MessageCache = new MessageCache();
 
-// export class Message {
-// public async reaction(req: Request, res: Response): Promise<void> {
-//   const { conversationId, messageId, reaction, type } = req.body;
-//   const updatedMessage: IMessageData = await messageCache.updateMessageReaction(
-//     `${conversationId}`,
-//     `${messageId}`,
-//     `${reaction}`,
-//     `${req.currentUser!.username}`,
-//     type
-//   );
+export class Message {
+public async reaction(req: Request, res: Response): Promise<void> {
+  const { conversationId, messageId, reaction, type } = req.body;
+  const updatedMessage: IMessageData = await messageCache.updateMessageReaction(
+    `${conversationId}`,
+    `${messageId}`,
+    `${reaction}`,
+    `${req.currentUser!.username}`,
+    type
+  );
 //   socketIOChatObject.emit('message reaction', updatedMessage);
 //   chatQueue.addChatJob('updateMessageReaction', {
 //     messageId: new mongoose.Types.ObjectId(messageId),
