ae8abea2d462462736c19cb799d037a65cabd46a
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-19 17:53:46 +0100

    search users
    Added search users

--- src/features/user/routes/userRoutes.ts
+++ src/features/user/routes/userRoutes.ts
@@ -19,7 +19,7 @@ class UserRoutes {
     this.router.get('/user/profile/:userId', authMiddleware.checkAuthentication, Get.prototype.singleUserProfile);
     this.router.get('/user/profile/posts/:username/:userId/:uId', authMiddleware.checkAuthentication, Get.prototype.profileAndPosts);
     this.router.get('/user/profile/user/suggestions', authMiddleware.checkAuthentication, Get.prototype.randomUserSuggestions);
-    // this.router.get('/user/profile/search/:query', authMiddleware.checkAuthentication, Search.prototype.user);
+    this.router.get('/user/profile/search/:query', authMiddleware.checkAuthentication, Search.prototype.user);
 
     // this.router.put('/user/profile/change-password', authMiddleware.checkAuthentication, Update.prototype.password);
     // this.router.put('/user/profile/basic-info', authMiddleware.checkAuthentication, Edit.prototype.info);
