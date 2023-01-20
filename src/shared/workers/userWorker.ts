99f050e20b11b21389c9296fd6a953dbfe9b2e5b
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-20 18:55:05 +0100

    Update userWorker.ts
    updateNotificationSettings

--- src/shared/workers/userWorker.ts
+++ src/shared/workers/userWorker.ts
@@ -42,17 +42,17 @@ class UserWorker {
     }
   }
 
-  // async updateNotificationSettings(job: Job, done: DoneCallback): Promise<void> {
-  //   try {
-  //     const { key, value } = job.data;
-  //     await userService.updateNotificationSettings(key, value);
-  //     job.progress(100);
-  //     done(null, job.data);
-  //   } catch (error) {
-  //     log.error(error);
-  //     done(error as Error);
-  //   }
-  // }
+  async updateNotificationSettings(job: Job, done: DoneCallback): Promise<void> {
+    try {
+      const { key, value } = job.data;
+      await userService.updateNotificationSettings(key, value);
+      job.progress(100);
+      done(null, job.data);
+    } catch (error) {
+      log.error(error);
+      done(error as Error);
+    }
+  }
 }
 
 export const userWorker: UserWorker = new UserWorker();
