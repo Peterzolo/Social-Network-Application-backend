f6456c02991042552dc0d091eea4ad6bcdc7e070
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-20 19:59:55 +0100

    Update editeUserInfo.ts
    Modified

--- src/features/user/controllers/editeUserInfo.ts
+++ src/features/user/controllers/editeUserInfo.ts
@@ -7,26 +7,25 @@ import { basicInfoSchema, socialLinksSchema } from '@user/schemes/userValidateSc
 
 const userCache: UserCache = new UserCache();
 
-// export class Edit {
-//   @joiValidation(basicInfoSchema)
-//   public async info(req: Request, res: Response): Promise<void> {
-//     for (const [key, value] of Object.entries(req.body)) {
-//       await userCache.updateSingleUserItemInCache(`${req.currentUser!.userId}`, key, `${value}`);
-//     }
-//     userQueue.addUserJob('updateBasicInfoInDB', {
-//       key: `${req.currentUser!.userId}`,
-//       value: req.body
-//     });
-//     res.status(HTTP_STATUS.OK).json({ message: 'Updated successfully' });
-//   }
-
-//   @joiValidation(socialLinksSchema)
-//   public async social(req: Request, res: Response): Promise<void> {
-//     await userCache.updateSingleUserItemInCache(`${req.currentUser!.userId}`, 'social', req.body);
-//     userQueue.addUserJob('updateSocialLinksInDB', {
-//       key: `${req.currentUser!.userId}`,
-//       value: req.body
-//     });
-//     res.status(HTTP_STATUS.OK).json({ message: 'Updated successfully' });
-//   }
-// }
+export class Edit {
+  @joiValidation(basicInfoSchema)
+  public async info(req: Request, res: Response): Promise<void> {
+    for (const [key, value] of Object.entries(req.body)) {
+      await userCache.updateSingleUserItemInCache(`${req.currentUser!.userId}`, key, `${value}`);
+    }
+    userQueue.addUserJob('updateBasicInfoInDB', {
+      key: `${req.currentUser!.userId}`,
+      value: req.body
+    });
+    res.status(HTTP_STATUS.OK).json({ message: 'Updated successfully' });
+  }
+  //   @joiValidation(socialLinksSchema)
+  //   public async social(req: Request, res: Response): Promise<void> {
+  //     await userCache.updateSingleUserItemInCache(`${req.currentUser!.userId}`, 'social', req.body);
+  //     userQueue.addUserJob('updateSocialLinksInDB', {
+  //       key: `${req.currentUser!.userId}`,
+  //       value: req.body
+  //     });
+  //     res.status(HTTP_STATUS.OK).json({ message: 'Updated successfully' });
+  //   }
+}
