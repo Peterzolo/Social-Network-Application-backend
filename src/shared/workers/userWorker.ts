19d8925cd2c2f6b0b9863ced329fe06cbeabd666
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-20 18:51:18 +0100

    Update userWorker.ts
    updateUserInfo

--- src/shared/workers/userWorker.ts
+++ src/shared/workers/userWorker.ts
@@ -18,17 +18,17 @@ class UserWorker {
     }
   }
 
-  // async updateUserInfo(job: Job, done: DoneCallback): Promise<void> {
-  //   try {
-  //     const { key, value } = job.data;
-  //     await userService.updateUserInfo(key, value);
-  //     job.progress(100);
-  //     done(null, job.data);
-  //   } catch (error) {
-  //     log.error(error);
-  //     done(error as Error);
-  //   }
-  // }
+  async updateUserInfo(job: Job, done: DoneCallback): Promise<void> {
+    try {
+      const { key, value } = job.data;
+      await userService.updateUserInfo(key, value);
+      job.progress(100);
+      done(null, job.data);
+    } catch (error) {
+      log.error(error);
+      done(error as Error);
+    }
+  }
 
   // async updateSocialLinks(job: Job, done: DoneCallback): Promise<void> {
   //   try {
