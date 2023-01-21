fc161f85ccb8a2a4c244105a7ab12f326c822f11
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-21 20:03:52 +0100

    Update getPosts.ts
    Modified

--- src/features/post/controllers/getPosts.ts
+++ src/features/post/controllers/getPosts.ts
@@ -41,10 +41,11 @@ export class Fetch {
     const { page } = req.params;
     const skip: number = (parseInt(page) - 1) * PAGE_SIZE;
     const limit: number = PAGE_SIZE * parseInt(page);
-    const newSkip: number = skip === 0 ? skip : skip + 1;
+    // const newSkip: number = skip === 0 ? skip : skip + 1;
     let posts: IPostDocument[] = [];
-    const cachedPosts: IPostDocument[] = await postCache.getPostsWithVideosFromCache('post', newSkip, limit);
-    posts = cachedPosts.length ? cachedPosts : await postService.getPosts({ videoId: '$ne' }, skip, limit, { createdAt: -1 });
+    // const cachedPosts: IPostDocument[] = await postCache.getPostsWithVideosFromCache('post', newSkip, limit);
+    // posts = cachedPosts.length ? cachedPosts : await postService.getPosts({ videoId: '$ne' }, skip, limit, { createdAt: -1 });
+    posts = await postService.getPosts({ videoId: '$ne' }, skip, limit, { createdAt: -1 });
     res.status(HTTP_STATUS.OK).json({ message: 'All posts with videos', posts });
   }
 }
