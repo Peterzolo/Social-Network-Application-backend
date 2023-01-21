24fa7b1e8be71ac3197765d3f8a3e2313c78f188
Peter Solomon <60029808+Peterzolo@users.noreply.github.com>
2023-01-21 08:07:46 +0100

    Update updateUserSetting.ts
    Modified

--- src/features/user/controllers/updateUserSetting.ts
+++ src/features/user/controllers/updateUserSetting.ts
@@ -7,14 +7,14 @@ import { notificationSettingsSchema } from '@user/schemes/userValidateSchema';
 
 const userCache: UserCache = new UserCache();
 
-// export class UpdateSettings {
-//   @joiValidation(notificationSettingsSchema)
-//   public async notification(req: Request, res: Response): Promise<void> {
-//     await userCache.updateSingleUserItemInCache(`${req.currentUser!.userId}`, 'notifications', req.body);
-//     userQueue.addUserJob('updateNotificationSettings', {
-//       key: `${req.currentUser!.userId}`,
-//       value: req.body
-//     });
-//     res.status(HTTP_STATUS.OK).json({ message: 'Notification settings updated successfully', settings: req.body });
-//   }
-// }
+export class UpdateSettings {
+  @joiValidation(notificationSettingsSchema)
+  public async notification(req: Request, res: Response): Promise<void> {
+    await userCache.updateSingleUserItemInCache(`${req.currentUser!.userId}`, 'notifications', req.body);
+    userQueue.addUserJob('updateNotificationSettings', {
+      key: `${req.currentUser!.userId}`,
+      value: req.body
+    });
+    res.status(HTTP_STATUS.OK).json({ message: 'Notification settings updated successfully', settings: req.body });
+  }
+}
